f58a82a29bc0a6a71f14033e94730d451717eb4b
Justin Reese <justin@justinreese.com>
2016-01-12 13:42:32 -0600

    Remove global `_` assignment
    This stomps on other Underscores and undermines the point of `DV._`  and `_.noConflict()`. Talked with @knowtheory, who advised taking it out until we understand what problem this was trying to solve.
    
    Addresses https://github.com/documentcloud/documentcloud/issues/212

--- public/javascripts/DV/lib/initializer.js
+++ public/javascripts/DV/lib/initializer.js
@@ -6,7 +6,6 @@ console.log    || (console.log = _.identity);
 window.DV   = window.DV   || {};
 DV.jQuery   = jQuery.noConflict(true);
 DV._        = _.noConflict(); // preserve a safe reference for the viewer
-_           = DV._; // make sure that _ is defined in the global namespace for now.
 DV.viewers  = DV.viewers  || {};
 DV.model    = DV.model    || {};
 
